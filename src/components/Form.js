f05c2e528dd5df824e0d7266259f39209965a430
James <jmcelroy@plexustech.net>
2020-08-10 11:34:39 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
-  }
+    lastName: "Henry",
+    submittedData: [],
+  };
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    let formData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+    };
 
-  handleLastNameChange = event => {
+    let dataArray = this.state.submittedData.concat(formData);
+  };
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map((data) => {
+      return (
+        <div>
+          <span>{data.firstName}</span> <span>{data.lastName}</span>
+        </div>
+      );
+    });
+  };
+
+  handleFirstNameChange = (event) => {
     this.setState({
-      lastName: event.target.value
-    })
-  }
+      firstName: event.target.value,
+    });
+  };
+
+  handleLastNameChange = (event) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      <form onSubmit={(event) => this.handleFormSubmit(event)}>
+        <input
+          type="text"
+          name="firstName"
+          onChange={(event) => this.handleFirstNameChange(event)}
+          value={this.state.firstName}
+        />
+        <input
+          type="text"
+          name="lastName"
+          onChange={(event) => this.handleLastNameChange(event)}
+          value={this.state.lastName}
+        />
+        <input type="submit" />
       </form>
-    )
+    );
   }
 }
 
